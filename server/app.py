18a0613b31a0507d722a21d7530b7589d3c27443
Ralph Shui <rskc.2112@gmail.com>
2023-08-21 09:31:25 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -47,9 +47,32 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self,id):
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        
+        db.session.add(plant)
+        db.session.commit()
+
+        plant_dict = plant.to_dict()
+        
+        response = make_response(plant_dict, 200)
+        return response
+    
+    def delete(self,id):
+        plant = Plant.query.filter_by(id=id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        response = make_response('', 204)
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
